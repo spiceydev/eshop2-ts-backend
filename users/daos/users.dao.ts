6fcea415dc5cb0fb40fe73ed4aa021476912d1d3
spiceydev <spiceydev@gmail.com>
2021-10-20 17:09:56 +1300

    configured the in memory db for testing

--- users/daos/users.dao.ts
+++ users/daos/users.dao.ts
@@ -12,4 +12,76 @@ class UsersDao {
   constructor() {
     log('Created new instance of UsersDao');
   }
+
+  async addUser(user: CreateUserDto) {
+    user.id = shortid.generate();
+    this.users.push(user);
+    return user.id;
+  }
+
+  async getUsers() {
+    return this.users;
+  }
+
+  async getUserById(userId: string) {
+    return this.users.find((user: { id: string }) => user.id === userId);
+  }
+
+  async putUserById(userId: string, user: PutUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+
+    this.users.splice(objIndex, 1, user);
+    return `${user.id} updated via put`;
+  }
+
+  async patchUserById(userId: string, user: PatchUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+
+    let currentUser = this.users[objIndex];
+    const allowedPatchFields = [
+      'email',
+      'passwordHash',
+      'permissionLevel',
+      'name',
+      'phone',
+      'street',
+      'postCode',
+      'city',
+      'country',
+    ];
+    for (let field of allowedPatchFields) {
+      if (field in user) {
+        // @ts-ignore
+        currentUser[field] = user[field];
+      }
+    }
+    this.users.splice(objIndex, 1, currentUser);
+    return `${user.id} patched`;
+  }
+
+  async removeUserById(userId: string) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+    this.users.splice(objIndex, 1);
+    return `${userId} removed`;
+  }
+
+  async getUserByEmail(email: string) {
+    const objIndex = this.users.findIndex(
+      (obj: { email: string }) => obj.email === email
+    );
+    let currentUser = this.users[objIndex];
+    if (currentUser) {
+      return currentUser;
+    } else {
+      return null;
+    }
+  }
 }
+
+export default new UsersDao();
