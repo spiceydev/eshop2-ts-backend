d3a99f71d10a8e319337c59251f068c5e73217e2
spiceydev <spiceydev@gmail.com>
2021-10-21 04:34:34 +1300

    added express validation

--- users/user.routes.config.ts
+++ users/user.routes.config.ts
@@ -3,6 +3,8 @@ import { CommonRoutesConfig } from '../common/common.routes.config';
 import express from 'express';
 import usersController from './controllers/users.controller';
 import usersMiddleware from './middleware/users.middleware';
+import { body } from 'express-validator';
+import bodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
     super(app, 'UsersRoutes');
@@ -13,7 +15,11 @@ export class UsersRoutes extends CommonRoutesConfig {
       .route('/users')
       .get(usersController.listUsers)
       .post(
-        usersMiddleware.validateRequiredUserBodyFields,
+        body('email').isEmail(),
+        body('password')
+          .isLength({ min: 5 })
+          .withMessage('Must include password (5+ characters)'),
+        bodyValidationMiddleware.verifyBodyFieldsErrors,
         usersMiddleware.validateSameEmailDoesNotExist,
         usersController.createUser
       );
@@ -26,12 +32,36 @@ export class UsersRoutes extends CommonRoutesConfig {
       .delete(usersController.removeUser);
 
     this.app.put('/users/:userId', [
-      usersMiddleware.validateRequiredUserBodyFields,
+      body('email').isEmail(),
+      body('password')
+        .isLength({ min: 5 })
+        .withMessage('Must include password (5+ characters)'),
+      body('name').isString(),
+      body('phone').isString(),
+      body('permissionFlags').isInt(),
+      body('street').isString(),
+      body('postCode').isString(),
+      body('city').isString(),
+      body('country').isString(),
+      bodyValidationMiddleware.verifyBodyFieldsErrors,
       usersMiddleware.validateSameEmailBelongToSameUser,
       usersController.put,
     ]);
 
     this.app.patch('/users/:userId', [
+      body('email').isEmail().optional(),
+      body('password')
+        .isLength({ min: 5 })
+        .withMessage('Password must be 5+ characters')
+        .optional(),
+      body('name').isString().optional(),
+      body('phone').isString().optional(),
+      body('permissionFlags').isInt().optional(),
+      body('street').isString().optional(),
+      body('postCode').isString().optional(),
+      body('city').isString().optional(),
+      body('country').isString().optional(),
+      bodyValidationMiddleware.verifyBodyFieldsErrors,
       usersMiddleware.validatePatchEmail,
       usersController.patch,
     ]);
